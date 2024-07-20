89bfd4c4d1cf231f787fe845428027ca15624916
salama135 <salamaa135@gmail.com>
2024-07-20 16:33:26 +0300

    feat: create application_model show

--- app/controllers/application_models_controller.rb
+++ app/controllers/application_models_controller.rb
@@ -2,4 +2,8 @@ class ApplicationModelsController < ApplicationController
   def index
     @application_models = ApplicationModel.all
   end
+
+  def show
+    @application_model = ApplicationModel.find(params[:id])
+  end
 end
