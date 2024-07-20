f49f592db194d353c37c3af992add6dfadd1850a
salama135 <salamaa135@gmail.com>
2024-07-20 23:36:56 +0300

    feat: edit application model - add validations - add 1 to many relation with chat_model

--- app/models/application_model.rb
+++ app/models/application_model.rb
@@ -1,2 +1,5 @@
 class ApplicationModel < ApplicationRecord
+    has_many :chat_models
+    validates :name, presence: true, uniqueness: true
+    validates :token, presence: true, uniqueness: true
 end
