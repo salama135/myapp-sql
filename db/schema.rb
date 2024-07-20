21825f10ca3280825f86c819defa7dc14e4684af
salama135 <salamaa135@gmail.com>
2024-07-21 00:46:21 +0300

    fix: apply migration using command rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_07_20_130953) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_20_202813) do
   create_table "application_models", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "token"
@@ -25,4 +25,25 @@ ActiveRecord::Schema[7.2].define(version: 2024_07_20_130953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "chat_models", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "application_model_id", null: false
+    t.integer "number"
+    t.integer "messages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["application_model_id"], name: "index_chat_models_on_application_model_id"
+  end
+
+  create_table "message_models", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "chat_model_id", null: false
+    t.integer "number"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_model_id"], name: "index_message_models_on_chat_model_id"
+  end
+
+  add_foreign_key "chat_models", "application_models"
+  add_foreign_key "message_models", "chat_models"
 end
