616f38fba7c497158400456aba8946bd1147167a
salama135 <salamaa135@gmail.com>
2024-07-21 22:34:55 +0300

    feat: update routes.rb
    - add application_models route
    - add chat_models route
    - add message_models route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "application_models#index"
+  # root "application_models#index"
 
-  get "/application_models", to: "application_models#index"
-  get "/application_models/:id", to: "application_models#show"
-
+  # get "/application_models", to: "application_models#index"
+  # get "/application_models/:id", to: "application_models#show"
+  resources :application_models, only: [:show, :create, :update] do
+    resources :chat_models, only: [:create, :show] do
+      resources :message_models, only: [:create, :show] do
+        collection do 
+          get 'search'
+        end
+      end
+    end
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
